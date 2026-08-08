89cdbaa40f54c836dd1c9c35079ba9493bea35c6
Suba79 <coolzaken13@gmail.com>
2026-08-08 16:13:05 +0300

    Remove unused code

--- blog/views.py
+++ blog/views.py
@@ -4,38 +4,11 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
 
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(
-            Post.objects.filter(tags=tag)
-        ),
-    }
-
 
 def serialize_tag_optimized(tag):
     return {
         'title': tag.title,
         'posts_with_tag': tag.posts_count,
-    }
-
-
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(
-            Comment.objects.filter(post=post)
-        ),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [
-            serialize_tag(tag)
-            for tag in post.tags.all()
-        ],
-        'first_tag_title': post.tags.all()[0].title,
     }
 
 
